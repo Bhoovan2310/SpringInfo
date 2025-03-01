cdd3fcec13ad1004e38a04086ad4d9e77f729c7c
Bhoovan Kumar <bhoovan2310@gmail.com>
2025-03-01 15:22:11 +0530

    springinfo first commit

---
+++ src/test/java/com/example/SpringInfo/SpringInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
